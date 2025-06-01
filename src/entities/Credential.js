53af484c1e2c627144dce47f769f2ef6d8761b24
joseph adogeri <70236136+jadogeri@users.noreply.github.com>
2025-06-01 02:24:16 -0500

    first commit

---
+++ src/entities/Credential.js
@@ -0,0 +1,35 @@
+export class Credential {
+    constructor(username, email, password){
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+    static default(){
+        new Credential("","","");
+
+    }
+    getEmail(){
+        return this.email
+    }
+
+    getUsername(){
+        return this.username
+    }
+
+    getPassword(){
+        return this.password
+    }
+
+    setEmail(email){
+        this.email = email;
+    }
+
+    setUsername(username){
+        this.username = username;
+    }
+
+    setPassword(password){
+        this.password = password;
+    }
+}
+
